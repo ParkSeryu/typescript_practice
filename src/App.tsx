61ce16d99d0ad7733f7771c5163fe1acf454f5fa
junHyeong <pjh971212@gmail.com>
2024-01-24 16:58:50 +0900

    type study add

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function 결혼가능하냐(소득: number, 집보유여부: boolean, 매력점수: string): string {
+        let 점수 = 소득;
+        if (집보유여부) {
+            점수 += 500;
+        }
+
+        if (매력점수 == '상') {
+            점수 += 100;
+        }
+
+        if (점수 >= 600)
+            return '결혼가능'
+
+        return 점수.toString();
+    }
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {결혼가능하냐(700, false, '중')}
+                {결혼가능하냐(100, false, '상')}
+            </header>
+        </div>
+    );
 }
 
 export default App;
