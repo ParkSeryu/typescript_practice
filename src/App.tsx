561ae8a3885fbb026baf9e84eda35fa0648cf073
ParkSeryu <pjh971212@gmail.com>
2024-01-30 21:36:21 +0900

    feat: 타입도 변수에 담아쓰세요 type 키워드 써서 & readonly

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,36 @@ import './App.css';
 
 function App() {
 
-    function 결혼가능하냐(소득: number, 집보유여부: boolean, 매력점수: string): string {
-        let 점수 = 소득;
-        if (집보유여부) {
-            점수 += 500;
-        }
-
-        if (매력점수 == '상') {
-            점수 += 100;
-        }
-
-        if (점수 >= 600)
-            return '결혼가능'
-
-        return 점수.toString();
+    type homework2 = {
+        color?: string,
+        size: number,
+        readonly position: number[]
     }
+
+    let test: homework2 = {
+        size: 123,
+        position: [1,2,3]
+    }
+
+    type homework3 = {
+        name: string,
+        phone: number
+        email: string
+    }
+
+    type homework4 = {
+        name: string,
+        phone: number,
+        email: string,
+        baby: boolean
+    }
+
+
 
 
     return (
         <div className="App">
             <header className="App-header">
-                {결혼가능하냐(700, false, '중')}
-                {결혼가능하냐(100, false, '상')}
             </header>
         </div>
     );
