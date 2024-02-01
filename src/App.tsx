16bda8565b31c7180a79c7e8f9e99bb124ad114d
ParkSeryu <pjh971212@gmail.com>
2024-02-01 20:44:54 +0900

    feat: 함수와 methods에 type alias 지정하는 법

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 function App() {
 
-    let 접니다: '대머리' | '솔로';
-    접니다 = "대머리";
+    type 함수타입 = (a: string) => number;
+    type NumOut = (x: number, y: number) => number;
 
-    function 함수(x: '가위' | '바위' | '보' ): ('가위' | '바위' | '보')[]{
-        return ['가위'];
+    let ABC: NumOut = function (x, y) {
+        return x + y;
     }
 
-    // literal type은 const 업글 버전과 비슷
+    type Member = {
+        name: string,
+        age: number,
+        plusOne: (x: number) => number,
+        changeName: () => void
+    };
 
-    함수('가위');
 
-    var 자료 = {
-         name : 'kim'
-    } as const
-
-
-    function 내함수(a: 'kim'){
-        return 자료.name
+    let 함수: 함수타입 = function (a) {
+        return 10;
     }
 
-    내함수(자료.name);
+    let 회원정보: Member = {
+        name: 'kim',
+        age: 30,
+        plusOne(x) {
+            return x + 1
+        },
+        changeName: () => {
+            console.log('안녕');
+        }
+    }
 
+    회원정보.plusOne(1);
+    // 회원정보.changeName();
+
+    type cut = (x: string) => string;
+    type remove = (x : string) => string;
+
+    function cutZero(x: string) {
+        if (x.charAt(0) == '0') {
+            return x.substring(1, x.length)
+        }
+        return x
+    }
+
+
+    function removeDash(x: string) {
+        let returnStr = "";
+        for (let i = 0; i < x.length; i++) {
+            if (x.charAt(i) != '-') {
+
+                returnStr += x.at(i);
+            }
+        }
+        return returnStr;
+    }
+
+
+    function 만들함수(x:string, y:cut, z: remove ):string{
+        return z(y(x));
+    }
+
+    console.log(만들함수('010-1111-2222', cutZero, removeDash));
+
+    //
+    // function 함수1(a) {
+    //     a()
+    // }
+    //
+    // function 함수2() {
+    //
+    // }
+    //
+    // 함수1(함수2)
 
     return (
         <div className="App">
