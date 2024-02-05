320c96fdae370b19c09dcc73d982540ecd5b84dc
ParkSeryu <pjh971212@gmail.com>
2024-02-05 20:27:21 +0900

    feat: 함수와 methods에 type alias 지정하는 법

--- src/App.tsx
+++ src/App.tsx
@@ -3,84 +3,39 @@ import './App.css';
 
 
 function App() {
-
-    type 함수타입 = (a: string) => number;
-    type NumOut = (x: number, y: number) => number;
-
-    let ABC: NumOut = function (x, y) {
-        return x + y;
+    let 제목 = document.querySelector('#title');
+    if (제목?.innerHTML != undefined) {
+        제목.innerHTML = '반가워요'
     }
 
-    type Member = {
-        name: string,
-        age: number,
-        plusOne: (x: number) => number,
-        changeName: () => void
-    };
+    let 링크 = document.querySelector('.link');
+    if (링크 instanceof HTMLAnchorElement)
+        링크.href = 'https://kakao.com'
 
+    let 버튼 = document.querySelector('#button');
+    let 이미지 = document.querySelector('#image');
+    버튼?.addEventListener('click', function () {
+        if (이미지 instanceof HTMLImageElement)
+            이미지.src = 'new.jpg'
+    })
 
-    let 함수: 함수타입 = function (a) {
-        return 10;
-    }
-
-    let 회원정보: Member = {
-        name: 'kim',
-        age: 30,
-        plusOne(x) {
-            return x + 1
-        },
-        changeName: () => {
-            console.log('안녕');
+    let 링크2 = document.querySelectorAll('.naver');
+    링크2.forEach((each) => {
+        if(each instanceof HTMLAnchorElement){
+            each.href='https://kakao.com'
         }
-    }
-
-    회원정보.plusOne(1);
-    // 회원정보.changeName();
-
-    type cut = (x: string) => string;
-    type remove = (x : string) => string;
-
-    function cutZero(x: string) {
-        if (x.charAt(0) == '0') {
-            return x.substring(1, x.length)
-        }
-        return x
-    }
-
-
-    function removeDash(x: string) {
-        let returnStr = "";
-        for (let i = 0; i < x.length; i++) {
-            if (x.charAt(i) != '-') {
-
-                returnStr += x.at(i);
-            }
-        }
-        return returnStr;
-    }
-
-
-    function 만들함수(x:string, y:cut, z: remove ):string{
-        return z(y(x));
-    }
-
-    console.log(만들함수('010-1111-2222', cutZero, removeDash));
-
-    //
-    // function 함수1(a) {
-    //     a()
-    // }
-    //
-    // function 함수2() {
-    //
-    // }
-    //
-    // 함수1(함수2)
+    })
 
     return (
         <div className="App">
-            <header className="App-header">
-            </header>
+            <h4 id="title">안녕하세요</h4>
+            <a href="naver.com" className="link">링크</a>
+            <button id="button">버튼</button>
+            <img id="image" src="test.jpg"></img>
+
+            <a className="naver" href="naver.com">링크</a>
+            <a className="naver" href="naver.com">링크</a>
+            <a className="naver" href="naver.com">링크</a>
         </div>
     );
 }
